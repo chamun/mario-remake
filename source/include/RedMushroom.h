40e728675f18126a105c457675d82beaebcf9007
Rodrigo Chamun <rochamun@gmail.com>
2013-12-01 16:28:11 -0200

    Red mushroom sprite

--- source/include/RedMushroom.h
+++ source/include/RedMushroom.h
@@ -8,18 +8,10 @@ class RedMushroom : public Collectable
 {
 	public:
 
-	RedMushroom(float x, float y, float width, float height) : Collectable(x, y, width, height) { }
-
-	void draw(sf::RenderTarget *target)
-	{
-		sf::RectangleShape shape(sf::Vector2<float>(getWidth() - marginx * 2, getHeight() - marginy * 2));
-		shape.setFillColor(sf::Color(0xFF, 0x50, 0x50, 0xFF));
-		shape.setOutlineColor(sf::Color(0xCC, 0x40, 0x40, 0xFF));
-		shape.setOutlineThickness(1);
-
-		sf::Vector2f npos = sf::Vector2f(getPosition().x + marginx, getPosition().y + marginy);
-		shape.setPosition(npos);
-		target->draw(shape);
+	RedMushroom(float x, float y, float width, float height) : Collectable(x, y, width, height)
+	{ 
+		sprite.loadXML((char *)"data/sprites/redMushroom.xml");
+		sprite.setPosition(getPosition());
 	}
 
     void actionOnContact(ActionHandler *handler) { handler->growPlayer(); }
